5a2dce2a83bac503454a34a0491364a76ac66a05
yovan <yovan.juggoo@chb-technologies.ch>
2016-08-29 10:48:49 +0400

    Initial commit

---
+++ Sites/accounts_manager/README.md
@@ -0,0 +1,2 @@
+# Using jquery UI in Ruby on Rails.
+This application was developed by Yovan Juggoo. 
---
+++ Sites/accounts_manager/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AccountsManager::Application.config.secret_token = '7e94038e43b6858e8ef30bf2f0ddb51172dee66f99185b42c800a091e4d201dd98ea2e163882dab16a2e3f92a6cf73066d205150953f076b2779e3a197d8a515'
